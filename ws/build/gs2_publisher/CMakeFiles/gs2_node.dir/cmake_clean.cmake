c7ef4b8e7e4cc00b943fa7cb3ddb60ed99897318
Emi Dretcanu <emidretcanu@u.boisestate.edu>
2025-10-20 10:31:11 -0600

    Added database

---
+++ ws/build/gs2_publisher/CMakeFiles/gs2_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gs2_node.dir/src/gs2_node.cpp.o"
+  "CMakeFiles/gs2_node.dir/src/gs2_node.cpp.o.d"
+  "gs2_node"
+  "gs2_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gs2_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ws/build/gs2_publisher/CMakeFiles/gs2_publisher_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gs2_publisher_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gs2_publisher_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
